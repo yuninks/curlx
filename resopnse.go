8bca4cc0134f7bd770ed132f15fc6b37aa987b25
Yun <yun@yun.ink>
2025-09-13 22:23:29 +0800

    更新

--- resopnse.go
+++ resopnse.go
@@ -93,7 +93,7 @@ func (r *Response) GetHeaders() map[string][]string {
 func (r *Response) GetHeader(name string) []string {
 	headers := r.GetHeaders()
 	for k, v := range headers {
-		if strings.ToLower(name) == strings.ToLower(k) {
+		if strings.EqualFold(name, k) {
 			return v
 		}
 	}
